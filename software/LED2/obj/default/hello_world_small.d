0f5a7e0f587e0fc971859ea6e8e5d67aa6f011ce
PavleTsotskolauri <36308387+PavleTsotskolauri@users.noreply.github.com>
2018-03-09 22:50:09 +0400

    Turn on LED

---
+++ software/LED2/obj/default/hello_world_small.d
@@ -0,0 +1,16 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../LED2_bsp//HAL/inc/sys/alt_stdio.h ../LED2_bsp/system.h \
+ ../LED2_bsp/linker.h ../LED2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../LED2_bsp//HAL/inc/io.h ../LED2_bsp//HAL/inc/alt_types.h
+
+../LED2_bsp//HAL/inc/sys/alt_stdio.h:
+
+../LED2_bsp/system.h:
+
+../LED2_bsp/linker.h:
+
+../LED2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../LED2_bsp//HAL/inc/io.h:
+
+../LED2_bsp//HAL/inc/alt_types.h:
